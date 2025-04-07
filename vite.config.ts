4e7f3a7c94b0e541227259289c172efd3e1c5d10
rubinafarhat01 <rubinafarhat09@gmail.com>
2025-04-07 17:30:12 +0500

    All file of eccomerce website

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+
+  /** This rule should be removed after new version of Bootstrap */
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler",
+        silenceDeprecations: ["legacy-js-api", "mixed-decls", "color-functions", "global-builtin", "import"],
+      },
+    },
+  },
+});
